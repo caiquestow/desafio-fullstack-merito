22e2ef442ca30cebd03ba65b078664d9bd4deafa
Caique <caique1_3@hotmail.com>
2025-06-09 15:39:07 -0300

    start

--- backend/investments/serializers.py
+++ backend/investments/serializers.py
@@ -11,25 +11,6 @@ class FundSerializer(serializers.ModelSerializer):
 class TransactionSerializer(serializers.ModelSerializer):
     fund_name = serializers.CharField(source='fund.name', read_only=True)
     
-    def validate(self, data):
-        if data['transaction_type'] == 'WITHDRAWAL':
-            # Calcula saldo atual
-            current_balance = self.get_current_balance()
-            if data['amount'] > current_balance:
-                raise serializers.ValidationError("Saldo insuficiente para resgate")
-        return data
-    
-    def get_current_balance(self):
-        deposits = Transaction.objects.filter(
-            transaction_type='DEPOSIT'
-        ).aggregate(Sum('amount'))['total'] or 0
-        
-        withdrawals = Transaction.objects.filter(
-            transaction_type='WITHDRAWAL'  
-        ).aggregate(Sum('amount'))['total'] or 0
-        
-        return deposits - withdrawals
-    
     class Meta:
         model = Transaction
         fields = '__all__'
